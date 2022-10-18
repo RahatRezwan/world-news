5272e768fa84e4e962905e308f48cdc2d59599cf
Kazi Rahat <kazirahat1007@outlook.com>
2022-10-18 07:09:22 +0600

    left side nav added

--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftSideNav = () => {
+   const [categories, setCategories] = useState([]);
+
+   useEffect(() => {
+      fetch("http://localhost:5000/categories")
+         .then((res) => res.json())
+         .then((data) => setCategories(data));
+   }, []);
+
+   console.log(categories);
    return (
       <div>
-         <h2>left side nav</h2>
+         <h4>All Categories {categories.length}</h4>
+         <div>
+            {categories.map((category) => (
+               <p key={category.id}>
+                  <Link to={`/category/${category.id}`}>{category.name}</Link>
+               </p>
+            ))}
+         </div>
       </div>
    );
 };
--- src/Pages/Shared/RightSideNav/RightSideNav.js
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -1,9 +1,13 @@
 import React from "react";
-
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
 const RightSideNav = () => {
    return (
       <div>
-         <h2>Right side nav</h2>
+         <ButtonGroup vertical>
+            <Button variant="outline-primary">Login with Google</Button>
+            <Button variant="outline-dark">Login with Github</Button>
+         </ButtonGroup>
       </div>
    );
 };
